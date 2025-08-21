e5788544a5d5d9abb9111e80acc556cabedf5a7f
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-08-21 09:17:44 -0400

    feat: saludar por genero

---
+++ src/genero.js
@@ -0,0 +1,13 @@
+function genero(gen)
+{
+    if (gen === "Femenino")
+    {
+        return "estimada";
+    }
+    else
+    {
+        return "estimado";
+    }
+}
+
+export default genero;
--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,8 @@
 import getHour from "./hora.js";
+import genero from "./genero.js";
 
 const name = document.querySelector("#nombre");
+const gen = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,14 +10,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nameValue = name.value;
+  const genValue = gen.value;
   const hora = getHour();
+  const genText = genero(genValue);
   
-  if (hora < 12) {
-    div.innerHTML = "<p>Buenos días "+ nameValue+"</p>";
+  if (hora < 12 && genText !== "error de género") {
+    div.innerHTML = "<p>Buenos días "+ genText + " " +nameValue+"</p>";
   } else if (hora < 18) {
-    div.innerHTML = "<p>Buenas tardes "+ nameValue+"</p>";
+    div.innerHTML = "<p>Buenas tardes "+ genText + " " +nameValue+"</p>";
   } else {
-    div.innerHTML = "<p>Buenas noches "+ nameValue+"</p>"; 
+    div.innerHTML = "<p>Buenas noches "+ genText + " " +nameValue+"</p>"; 
   }
 
   // div.innerHTML = "<p>Hola " + nameValue + "</p>";
