59efbb89eb6654eef96548cb0a8d0842c8f127ad
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-08-21 08:36:11 -0400

    feat: saludar por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,13 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const name = document.querySelector("#nombre");
+const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const nameValue = name.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Hola " + nameValue + "</p>";
 });
